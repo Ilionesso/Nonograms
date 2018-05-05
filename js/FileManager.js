c5a8d0ae95ac2835f71383e8efad8212deaed989
Ilia Sheiko <sheikili@fel.cvut.cz>
2018-05-05 23:30:44 +0200

    One more manifest fix???

--- js/FileManager.js
+++ js/FileManager.js
@@ -8,6 +8,14 @@ class FileManager{
                 main.setLevels(json);
                 console.log("Levels are gotten");
                 main.chooseLevel();
-            });
+            }).catch(() =>{
+            fetch("./resources/levels/offlineLevels.json")
+                .then(response => response.json())
+                .then(json => {
+                    main.setLevels(json);
+                    console.log("Offline levels are gotten");
+                    main.chooseLevel();
+                });
+        });
     }
 }
